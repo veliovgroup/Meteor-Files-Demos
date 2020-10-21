ce9e457488aedb7a2c7e975c61cb79dfe195e868
dr.dimitru <ceo@veliov.com>
2020-10-22 00:39:21 +0200

    Disallow remove from client, better error message about upload size

--- demo/imports/client/files.collection.js
+++ demo/imports/client/files.collection.js
@@ -7,17 +7,15 @@ import { FilesCollection } from 'meteor/ostrio:files';
 Collections._files = new Mongo.Collection(null);
 Collections._files._name = 'uploadedFiles';
 
-console.log(_app.conf)
-
 Collections.files = new FilesCollection({
   debug: true,
   collection: Collections._files,
-  allowClientCode: true,
+  allowClientCode: false,
   // disableUpload: true,
   onBeforeUpload() {
     if (this.file.size <= _app.conf.maxFileSize) {
       return true;
     }
-    return `Max. file size is ${filesize(_app.conf.maxFileSize)} you've tried to upload ${filesize(this.file.size)}`;
+    return `Max. file size is ${filesize(_app.conf.maxFileSize).replace('.00', '')} you've tried to upload ${filesize(this.file.size)}`;
   }
 });
