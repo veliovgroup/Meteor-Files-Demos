563b55f1f58803734f435f3883aed270642ec10a
dr.dimitru <ceo@veliov.com>
2020-10-29 16:59:57 +0100

    👷 Move to the new repo
    Move to veliovgroup/meteor-files-website

--- demo/imports/server/spiderable.js
+++
@@ -1,8 +0,0 @@
-// import { WebApp } from 'meteor/webapp';
-// import Spiderable from 'meteor/ostrio:spiderable-middleware';
-//
-// WebApp.connectHandlers.use(new Spiderable({
-//   rootURL: 'https://files.veliov.com',
-//   serviceURL: 'https://render.ostr.io',
-//   auth: 'xxx:yyy'
-// }));
--- demo/server/core.js
+++
@@ -1,3 +0,0 @@
-import '/imports/server/files.collection.js';
-import '/imports/server/methods.js';
-import '/imports/server/spiderable.js';
